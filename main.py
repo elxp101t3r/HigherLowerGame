9d7b364bee005fccee1655b8c8fefa3abfb67185
elxp101t3r <eliotx10101@outlook.com>
2023-11-03 17:47:22 +0200

    Comparison functionality

--- main.py
+++ main.py
@@ -31,5 +31,16 @@ def play(data):
                 os.system('clear')
                 print(logo)
                 print(f"Sorry, you are wrong. Final score: {final_score}")
-        
+        elif user_choice == 'B':
+            if followers_b > followers_a:
+                final_score += 1
+                os.system('clear')
+                print(logo)
+                print(f"You are right! Final score: {final_score}")
+            elif followers_b < followers_a:
+                os.system('clear')
+                print(logo)
+                print(f"Sorry, you are wrong. Final score: {final_score}")
+    comparison(followers_a=followers_a, followers_b=followers_b)
+    
 play(data)
